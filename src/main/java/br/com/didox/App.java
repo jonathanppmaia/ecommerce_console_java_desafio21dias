cd600a331782df297e7548cf48368137918fed17
jonathanppmaia <jonathanppmaia@gmail.com>
2023-07-09 20:33:58 -0300

    primeiro exercicio

--- src/main/java/br/com/didox/App.java
+++ src/main/java/br/com/didox/App.java
@@ -1,13 +1,48 @@
 package br.com.didox;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        int numero1 = 3;
-        var numero2 = 5;
-        int soma = numero1 + numero2;
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(System.in));
 
-        // mostre() #mostra informação na tela
-        System.out.println("O resultado da operação é " + soma);
+
+        /*
+            *EXERCÍCIO 01
+            * João é um professor de matemática e precisa de um programa que faça o calculo da seguinte expressão:
+            * (x * 8 + y) -x +y / 100
+            * faça um programa que resolva o probelma da expressão qeu o joão precisa.
+            * Lembre-se : pense na usabilidade !!
+        */
+
+        
+
+        /*   // Mostre()
+        System.out.println("Digite um número");
+
+        // Leia()
+        int numero = Integer.parseInt(reader.readLine());
+
+        var resultado = numero + 1000;
+        // Mostre()
+        System.out.println("O resultado de (" + numero + ") + 1000 é de : " + resultado); */
+
+
+        /*
+         * // Reading daa using readLine
+         * String name = reader.readLine();
+         * 
+         * int numero1 = 3;
+         * var numero2 = 5;
+         * 
+         * int soma = numero1 + numero2;
+         * 
+         * mostre() #mostra informação na tela
+         * System.out.println("O resultado da operação é " + soma);
+         */
     }
 }

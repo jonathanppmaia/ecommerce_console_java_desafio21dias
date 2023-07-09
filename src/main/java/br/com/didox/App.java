b225f61810cd401ab031ba23e126c866a580e7b1
jonathanppmaia <jonathanppmaia@gmail.com>
2023-07-09 17:40:13 -0300

    forçando aplicação para rodar no java 11

--- src/main/java/br/com/didox/App.java
+++ src/main/java/br/com/didox/App.java
@@ -1,13 +1,13 @@
 package br.com.didox;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+
+        int numero1 = 3;
+        var numero2 = 5;
+        int soma = numero1 + numero2;
+
+        // mostre() #mostra informação na tela
+        System.out.println("O resultado da operação é " + soma);
     }
 }

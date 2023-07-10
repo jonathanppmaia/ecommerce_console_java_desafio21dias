4a246fd855605815921f2ce0ab6ec982fb9e07b0
jonathanppmaia <jonathanppmaia@gmail.com>
2023-07-09 21:38:34 -0300

    logica para calculo de expressão

--- src/main/java/br/com/didox/App.java
+++ src/main/java/br/com/didox/App.java
@@ -9,28 +9,43 @@ public class App {
 
         BufferedReader reader = new BufferedReader(
                 new InputStreamReader(System.in));
+        /*
+         * EXERCÍCIO 01
+         * João é um professor de matemática e precisa de um programa que faça o calculo
+         * da seguinte expressão:
+         * (x * 8 + y) -x +y / 100
+         * faça um programa que resolva o probelma da expressão qeu o joão precisa.
+         * Lembre-se : pense na usabilidade !!
+         */
 
+        System.out.println("==========================================");
+        System.out.println("Olá, seja muito bem vindo!");
+        System.out.println("==========================================");
+        System.out.println("Notei que você quer resolver está operação");
+        System.out.println("(x * 8 + y) - x + y / 100 ");
+        System.out.println("Para isso preciso que você digite o valor de x e y, então:");
+        System.out.println("Digite o Valor de X: ");
+        int x = Integer.parseInt(reader.readLine());
+        System.out.println("Digite o Valor de y: ");
+        int y = Integer.parseInt(reader.readLine());
+        var operacao = (x * 8 + y) - x + y / 100;
+
+        System.out.println("O resultado do calculo da operação:");
+        System.out.println("(" + x + " * 8 + " + y + ") - " + x + " + " + y + " / 100 ");
+        System.out.println("Foi " + operacao);
 
         /*
-            *EXERCÍCIO 01
-            * João é um professor de matemática e precisa de um programa que faça o calculo da seguinte expressão:
-            * (x * 8 + y) -x +y / 100
-            * faça um programa que resolva o probelma da expressão qeu o joão precisa.
-            * Lembre-se : pense na usabilidade !!
-        */
-
-        
-
-        /*   // Mostre()
-        System.out.println("Digite um número");
-
-        // Leia()
-        int numero = Integer.parseInt(reader.readLine());
-
-        var resultado = numero + 1000;
-        // Mostre()
-        System.out.println("O resultado de (" + numero + ") + 1000 é de : " + resultado); */
-
+         * // Mostre()
+         * System.out.println("Digite um número");
+         * 
+         * // Leia()
+         * int numero = Integer.parseInt(reader.readLine());
+         * 
+         * var resultado = numero + 1000;
+         * // Mostre()
+         * System.out.println("O resultado de (" + numero + ") + 1000 é de : " +
+         * resultado);
+         */
 
         /*
          * // Reading daa using readLine
